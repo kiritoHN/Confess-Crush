ef29b7c831d21f5065eb328de13475bb144b1ace
kiritoHN <97202792+kiritoHN@users.noreply.github.com>
2022-01-29 11:04:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,8 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
+    titleWeb: "Em có yêu anh khônggg",
+    introTitle: 'Ly',
+    introDesc: `Nhiều người đang nghĩ là em không phải bạn gái anh thì em có muốn chứng minh họ sai với anh ko :v`,
+    btnIntro: ':)))))',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
